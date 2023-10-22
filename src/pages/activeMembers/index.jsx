f445ba3b85035ca23c878e0f22ceff8a02c00bd6
ALI NAEEM <137595889+Runty786@users.noreply.github.com>
2023-10-22 17:58:53 +0500

    add section 2 activemember

--- src/pages/activeMembers/index.jsx
+++ src/pages/activeMembers/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Section2 from './Section2/ActiveMember'
+import Section1 from './Section2/MemberData'
 
 const Index = () => {
-  return <div>Index</div>
+  return (
+    <>
+    
+     <Section2 />
+    </>
+  )
+  
+
+  
 }
 
 export default Index
--- src/pages/pricing/index.jsx
+++ src/pages/pricing/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Index = () => {
-  return <div>Index</div>
+  return <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ipsam commodi in quis aliquam dolorem laborum eius dicta asperiores vel dolores totam omnis, tenetur aut nam, sint qui corporis deleniti!</p></div>
 }
 
 export default Index
