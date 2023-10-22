9cc7d20473fa99f17c7c365b65886ccc6574017f
Ahmad <ahmad03065917176@gmail.com>
2023-10-22 18:54:24 +0500

    index of activemember

--- src/pages/activeMembers/index.jsx
+++ src/pages/activeMembers/index.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Section2 from "./Section2/ActiveMember";
 import Section1 from "./Section2/MemberData";
-
-import Section1 from "./Section1/Section1";
 const Index = () => {
   return (
-    <div>
-      <>
-        <Section1 />
-        <Section2 />
-      </>
-    </div>
+    <>
+      <Section1 />
+      <Section2 />
+    </>
   );
 };
 
