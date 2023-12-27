acea3e493f14ec93f075cf632461be36cccc4680
ALI NAEEM <137595889+Runty786@users.noreply.github.com>
2023-12-27 11:57:57 +0500

    Add profilabe changes

--- src/pages/Home2/index.jsx
+++ src/pages/Home2/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Loginpage from './loginpage/Loginpage'
 import  Forgotpass  from './ForgotPassword/Forgotpass'
-import Profileable from './loginpage/Profileable'
+// import Profileable from './loginpage/Profileable'
+import IndexProfile from "../Home2/Profile/indexProfile";
 const index = () => {
   return (
     <>
-    {/* <Loginpage/> */}
-    {/* <Forgotpass/> */}
-    <Profileable/>
+    <Loginpage/>
+    <Forgotpass/>
+    {/* <Profileable/> */}
+    <IndexProfile />
+
   
     </>
   )
