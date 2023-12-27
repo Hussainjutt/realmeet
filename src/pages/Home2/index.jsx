76994cc61d065e634353b85a9e8aeb57c8e697c7
ALI NAEEM <137595889+Runty786@users.noreply.github.com>
2023-12-27 11:22:55 +0500

    profileable

--- src/pages/Home2/index.jsx
+++ src/pages/Home2/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Loginpage from './loginpage/Loginpage'
 import  Forgotpass  from './ForgotPassword/Forgotpass'
+import Profileable from './loginpage/Profileable'
 const index = () => {
   return (
     <>
     {/* <Loginpage/> */}
-    <Forgotpass/>
+    {/* <Forgotpass/> */}
+    <Profileable/>
   
     </>
   )
---
+++ src/pages/Home2/loginpage/Profileable.jsx
@@ -0,0 +1,235 @@
+import React from "react";
+
+const Profileable = () => {
+  return (
+    <>
+
+      <div className="w-[80%] mx-auto">
+      <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-3 sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+              <table class="min-w-full border  text-sm font-light ">
+                <tbody>
+                  <tr class="border-b ">
+                    <td class=" border-r  px-11 py-11 font-medium ">
+                      <p className=" bold opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                        General: 
+                      </p>
+                      <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                        Ernest K Cole
+                      </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4     ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                  Residence Country:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                  American Samoa
+                </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                    <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                  Height:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                  5’ 6’’
+                </h3>
+                    </td>
+                  </tr>
+                  <tr class="border-b ">
+                    <td class=" border-r px-6 py-4 font-medium ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                        Weight:
+                      </p>
+                      <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                      60 KG
+                      </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Weight:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                60 KG
+                </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                    <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                    Marital Status
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                Never Married
+                </h3>
+                    </td>
+                  </tr>
+                  <tr class="border-b ">
+                    <td class=" border-r px-6 py-4 font-medium ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Children Acceptable:
+                      </p>
+                      <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                      Yes
+                      </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Religion:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                Judaismm
+                </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                    <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                    Caste:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                  Muslim
+                </h3>
+                    </td>
+                  </tr>
+                  <tr class="border-b ">
+                    <td class=" border-r px-6 py-4 font-medium ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Sub Caste:
+                      </p>
+                      <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                        Never Married
+                      </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Language:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                English
+                </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                    <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                  Education
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                Bachelor Of CS
+                </h3>
+                    </td>
+                  </tr>
+                  <tr class="border-b ">
+                    <td class=" border-r px-6 py-4 font-medium ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Profession:
+                      </p>
+                      <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                        Web Developer
+                      </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Smoking Acceptable:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                Dose not Matter
+                </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                    <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                    Drinking Acceptable:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                Dose not Matter
+                </h3>
+                    </td>
+                  </tr>
+                  <tr class="border-b ">
+                    <td class=" border-r px-6 py-4 font-medium ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Diet:
+                      </p>
+                      <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                      Dose not Matter
+                      </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Body Type::
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                Regular
+                </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                    <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                    Drinking Acceptable:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                Dose not Matter
+                </h3>
+                    </td>
+                  </tr>
+                  <tr class="border-b ">
+                    <td class=" border-r px-6 py-4 font-medium ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                        General:
+                      </p>
+                      <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                        Ernest K Cole
+                      </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                  Residence Country:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                  American Samoa
+                </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                    <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                  Height:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                  5’ 6’’
+                </h3>
+                    </td>
+                  </tr>
+                  <tr class="border-b ">
+                    <td class=" border-r px-11 py-11 font-medium ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Family Value:
+                      </p>
+                      <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                      Xanthous Collins
+                      </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                      <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+                      Complexion:
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                Fair skin
+                </h3>
+                    </td>
+                    <td class=" border-r px-6 py-4 ">
+                    <p className="opacity-70 text-slate-800 text-base font-normal font-['BR Omega']">
+            
+                </p>
+                <h3 className="text-slate-800 text-2xl font-normal font-['BR Omega']">
+                  
+                </h3>
+                    </td>
+                  </tr>
+                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      
+    </>
+  );
+};
+
+export default Profileable;
